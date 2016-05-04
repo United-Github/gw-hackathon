a55859ccae7a56d74511e993a769a1e88d0b457c
rarog <ragrog216@outlook.jp>
2016-05-04 16:06:47 +0900

    Resultクラスを作成

--- Pomodoro/app/src/main/java/org/yuta/pomodoro/MainActivity.java
+++ Pomodoro/app/src/main/java/org/yuta/pomodoro/MainActivity.java
@@ -10,26 +10,12 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String[] texts = {
-            "abc ", "bcd", "cde", "def", "efg",
-            "fgh", 	"ghi", "hij", "ijk", "jkl",
-            "klm"
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-
-        ListView listView = new ListView(this);
-        setContentView(R.layout.result_main);
-        ListView listView1 = (ListView)findViewById(R.id.listView);
-        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, texts);
-
-        listView1.setAdapter(arrayAdapter);
 
 
     }
