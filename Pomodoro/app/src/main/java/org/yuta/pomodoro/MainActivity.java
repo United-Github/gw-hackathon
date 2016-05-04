25c49a51ef05e4cf39ec50f59facb81705bdc469
rarog <ragrog216@outlook.jp>
2016-05-04 16:00:51 +0900

    Result画面

--- Pomodoro/app/src/main/java/org/yuta/pomodoro/MainActivity.java
+++ Pomodoro/app/src/main/java/org/yuta/pomodoro/MainActivity.java
@@ -2,12 +2,35 @@ package org.yuta.pomodoro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] texts = {
+            "abc ", "bcd", "cde", "def", "efg",
+            "fgh", 	"ghi", "hij", "ijk", "jkl",
+            "klm"
+    };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        ListView listView = new ListView(this);
+        setContentView(R.layout.result_main);
+        ListView listView1 = (ListView)findViewById(R.id.listView);
+        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, texts);
+
+        listView1.setAdapter(arrayAdapter);
+
+
     }
 }
